102292f235f0215444a689bb5e041c5fd092a792
unknown <fdsa4321jkl@yahoo.com>
2019-03-07 15:02:30 -0800

    second skills check basic js

---
+++ index.js
@@ -0,0 +1,25 @@
+//Create variable called lovesCode and set it true or false depending on if you love code or not. It's okay if you don't, yet
+
+let lovesCode = true;
+
+//Create a variable called age that is your age as a number
+
+let age = 25;
+
+//Create a variable called name that is your name as a string
+
+let name = "my name";
+
+//Create a variable called phrase that is initially set as an empty string
+
+let phrase = "";
+
+//create an if statement that checkes to see if the lovesCode variable is equal to true, if it is, set the phrase to 'I love code', it it's not true set the phrase to 'I do not love to code'
+
+if (lovesCode){
+  phrase = "I love code";
+} else {
+  phrase = "I do not love to code";
+}
+
+// Pres the big green `run` button above to see your project output on the right side.
